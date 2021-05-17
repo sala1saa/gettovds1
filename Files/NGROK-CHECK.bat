ae11785f1579cc54629074a27968ad2b2908b229
hayday1 <82230066+hayday1@users.noreply.github.com>
2021-05-17 10:52:33 +0300

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -5,7 +5,6 @@ REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" 
 net user administrator Gettovds12 /add >nul
 net localgroup administrators administrator /add >nul
 echo Hepsi tamam! VDS'nizi Uzak Masaüstü Bağlantısı kullanarak bağlayın
-echo IP: Yok
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
 echo Kullanıcı Adı: Administrator
 echo Şifre: Gettovds12
